77df575d1eedfce2a303768518a15e767b39e414
yblim <dudqls28@naver.com>
2022-04-01 17:02:13 +0900

    commit

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useRef,useState} from 'react';
+import styled from "styled-components/native";
+import { Animated, PanResponder } from "react-native";
 
+const Container = styled.View`
+  flex:1;
+  justify-content : center;
+  align-items: center;
+`;
+
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 export default function App() {
+  const [up,setUp] = useState(false);
+  const POSITION = useRef(new Animated.ValueXY({x:0,y:0})).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(POSITION,{
+      toValue: up ? 300 : -300,
+      duration:1000,
+      useNativeDriver:false,
+    }).start(toggleUp);
+  };
+  /*const opacity = POSITION.y.interpolate({
+    inputRange: [-300,0,300],
+    outputRange : [1,0.5,1],
+  });*/
+  const rotation = POSITION.y.interpolate({
+    inputRange:[-300,300],
+    outputRange:["-540deg","540deg"],
+  });
+  const bgColor = POSITION.y.interpolate({
+    inputRange:[-300,300],
+    outputRange:["rgb(255,99,71)","rgb(73,152,223)"],
+  })
+  const borderRadius = POSITION.y.interpolate({
+    inputRange: [-300,300],
+    outputRange : [100,0],
+  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove : (_,{dx,dy}) => {
+        POSITION.setValue({
+          x: dx,
+          y: dy,
+        });
+      }
+    })
+  ).current;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Container>
+        <AnimatedBox 
+          {...panResponder.panHandlers}
+          style={{
+            borderRadius,
+            backgroundColor: bgColor,
+            transform: [...POSITION.getTranslateTransform()],
+          }}
+        />
+    </Container>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
